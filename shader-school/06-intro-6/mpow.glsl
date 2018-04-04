e86df427b2e396923107a29900df56200609ff40
Luis Del Águila <luisdelaguila@outlook.com>
2018-04-04 00:17:00 -0600

    add shader-school exercises up to intro-6

--- shader-school/06-intro-6/mpow.glsl
+++ shader-school/06-intro-6/mpow.glsl
@@ -1,13 +1,26 @@
 mat2 matrixPower(highp mat2 m, int n) {
-  
-  //Raise the matrix m to nth power
+  if (n == 0) {
+    return m * mat2(1.0);
+  }
 
-  // For example:
-  //
-  //  matrixPower(m, 2) = m * m
-  //
+  if (n == 1) {
+    return m;
+  }
 
-  return mat2(1.0);  
+  // GLSL loops are un-rolled into native GPU instructions.
+  // This means there needs to be a hard upper limit to the number of passes
+  // through the for loop
+  const int MAX_N = 20;
+
+  for (int i = 0; i < MAX_N; i += 1) {
+    if (i >= n) {
+      break;
+    }
+
+    m = m * m;
+  }
+
+  return m;
 }
 
 //Do not change this line or the name of the above function
